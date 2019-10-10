848a86b7bd6341e562285fcae57dbd085f6936d8
vincent servio <vincentservio@gmail.com>
2019-10-10 22:10:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,9 +48,8 @@ cart
 end
 
 def checkout(cart, coupons)
- consol_cart = consolidate_cart(cart)
-  applied_coupons = apply_coupons(consol_cart, coupons)
-    applied_discount = apply_clearance(applied_coupons)
+ cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
  
  total = 0.0
   cart_with_discounts_applied.keys.each do |item|
